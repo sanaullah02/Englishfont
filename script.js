e2144232abe2edae22265fa8ab0502f13ddde630
sanaullah02 <103077099+sanaullah02@users.noreply.github.com>
2022-04-06 03:33:20 +0500

    English fonts
    Font styles for my business

---
+++ script.js
@@ -0,0 +1,81 @@
+function changeText() {
+    var els = document.getElementsByClassName("ribbon"); for (var i=0; i<els.length; i++) { els[i].style.display = "block"; }
+    var x = document.getElementById("text01");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Qwigley</p>";
+	var x = document.getElementById("text02");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Italianno</p>";
+	var x = document.getElementById("text03");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Arizonia</p>";
+	var x = document.getElementById("text04");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Brush Script</p>";
+	var x = document.getElementById("text05");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Alex Brush</p>";
+	var x = document.getElementById("text06");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">MilasianCirca</p>";
+	var x = document.getElementById("text07");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Great Vibes</p>";
+	var x = document.getElementById("text08");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Freebooter Script</p>";
+	var x = document.getElementById("text09");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Scriptina</p>";
+	var x = document.getElementById("text10");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Precious</p>";
+	var x = document.getElementById("text11");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">168-CAI978</p>";
+	var x = document.getElementById("text12");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Holy Union</p>";
+	var x = document.getElementById("text13");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Aquarelle</p>";
+	var x = document.getElementById("text14");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Allura</p>";
+	var x = document.getElementById("text15");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Lovers Quarrel</p>";
+	var x = document.getElementById("text16");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Love</p>";
+	var x = document.getElementById("text17");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">BlackSword</p>";
+	var x = document.getElementById("text18");
+    x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Halimun</p>";
+}
+
+function changeToGold() {
+     document.getElementById("text01").style.color = "#DCCD80";
+     document.getElementById("text02").style.color = "#DCCD80";
+     document.getElementById("text03").style.color = "#DCCD80";
+     document.getElementById("text04").style.color = "#DCCD80";
+     document.getElementById("text05").style.color = "#DCCD80";
+     document.getElementById("text06").style.color = "#DCCD80";
+     document.getElementById("text07").style.color = "#DCCD80";
+     document.getElementById("text08").style.color = "#DCCD80";
+     document.getElementById("text09").style.color = "#DCCD80";
+     document.getElementById("text10").style.color = "#DCCD80";
+     document.getElementById("text11").style.color = "#DCCD80";
+     document.getElementById("text12").style.color = "#DCCD80";
+     document.getElementById("text13").style.color = "#DCCD80";
+     document.getElementById("text14").style.color = "#DCCD80";
+     document.getElementById("text15").style.color = "#DCCD80";
+     document.getElementById("text16").style.color = "#DCCD80";
+	 document.getElementById("text17").style.color = "#DCCD80";
+	 document.getElementById("text18").style.color = "#DCCD80";
+}
+
+function changeToSilver() {
+     document.getElementById("text01").style.color = "#C9D1D3";
+     document.getElementById("text02").style.color = "#C9D1D3";
+     document.getElementById("text03").style.color = "#C9D1D3";
+     document.getElementById("text04").style.color = "#C9D1D3";
+     document.getElementById("text05").style.color = "#C9D1D3";
+     document.getElementById("text06").style.color = "#C9D1D3";
+     document.getElementById("text07").style.color = "#C9D1D3";
+     document.getElementById("text08").style.color = "#C9D1D3";
+     document.getElementById("text09").style.color = "#C9D1D3";
+     document.getElementById("text10").style.color = "#C9D1D3";
+     document.getElementById("text11").style.color = "#C9D1D3";
+     document.getElementById("text12").style.color = "#C9D1D3";
+     document.getElementById("text13").style.color = "#C9D1D3";
+     document.getElementById("text14").style.color = "#C9D1D3";
+     document.getElementById("text15").style.color = "#C9D1D3";
+     document.getElementById("text16").style.color = "#C9D1D3";
+	 document.getElementById("text17").style.color = "#C9D1D3";
+	 document.getElementById("text18").style.color = "#C9D1D3";
+}
